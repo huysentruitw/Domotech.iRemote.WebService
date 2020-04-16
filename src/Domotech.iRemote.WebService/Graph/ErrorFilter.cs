c6385c61e08c0f6c8df640ee1a544eb171d716d5
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2020-04-16 17:44:56 +0200

    Add custom ErrorFilter for HotChocolate

---
+++ src/Domotech.iRemote.WebService/Graph/ErrorFilter.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using HotChocolate;
+
+namespace Domotech.iRemote.WebService.Graph
+{
+    internal sealed class ErrorFilter : IErrorFilter
+    {
+        private static readonly Regex SnakeCasingRegex = new Regex("([A-Z])", RegexOptions.Compiled);
+
+        public IError OnError(IError error)
+        {
+            if (error.Exception == null)
+                return error;
+
+            string code = GetCodeForException(error.Exception);
+            IReadOnlyDictionary<string, object> data = GetExceptionData(error.Exception);
+
+            return error
+                .WithCode(code)
+                .AddExtension("data", data);
+        }
+
+        private static string GetCodeForException(Exception exception)
+        {
+            var exceptionName = exception.GetType().Name;
+
+            if (exceptionName.EndsWith("Exception"))
+                exceptionName = exceptionName[..^9];
+
+            return ToSnakeCasing(exceptionName);
+        }
+
+        private static IReadOnlyDictionary<string, object> GetExceptionData(Exception exception)
+        {
+            var data = new Dictionary<string, object>();
+
+            Type exceptionType = exception.GetType();
+            foreach (PropertyInfo propertyInfo in exceptionType.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+                data.Add(ToCamelCasing(propertyInfo.Name), propertyInfo.GetValue(exception));
+
+            return data;
+        }
+
+        private static string ToSnakeCasing(string input)
+            => SnakeCasingRegex.Replace(input, "_$0").TrimStart('_').ToUpper();
+
+        private static string ToCamelCasing(string input)
+            => char.ToLower(input[0]) + input.Substring(1);
+    }
+}
--- src/Domotech.iRemote.WebService/Startup.cs
+++ src/Domotech.iRemote.WebService/Startup.cs
@@ -21,6 +21,8 @@ namespace Domotech.iRemote.WebService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddErrorFilter<ErrorFilter>();
+
             services.AddGraphQL(serviceProvider => SchemaBuilder.New()
                 .AddQueryType<Query>()
                 .AddMutationType<Mutation>()

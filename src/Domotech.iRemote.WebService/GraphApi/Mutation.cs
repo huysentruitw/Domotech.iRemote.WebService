46dc4d6cd373360ad559ca19ab347871cff399d1
Wouter Huysentruit <wouter_huysentruit@hotmail.com>
2020-04-18 14:43:51 +0200

    Update Query and Mutation class

--- src/Domotech.iRemote.WebService/GraphApi/Mutation.cs
+++ src/Domotech.iRemote.WebService/GraphApi/Mutation.cs
@@ -4,14 +4,22 @@ namespace Domotech.iRemote.WebService.GraphApi
 {
     public sealed class Mutation
     {
-        public LightMutation Light(int id) => new LightMutation(id);
+        public AudioZoneMutation AudioZone(int id) => new AudioZoneMutation(id);
 
         public DimmableLightMutation DimmableLight(int id) => new DimmableLightMutation(id);
 
-        public SocketMutation Socket(int id) => new SocketMutation(id);
+        public LightMutation Light(int id) => new LightMutation(id);
+
+        public SceneMutation Scene(int id) => new SceneMutation(id);
 
         public ShutterMutation Shutter(int id) => new ShutterMutation(id);
 
-        public SceneMutation Scene(int id) => new SceneMutation(id);
+        public SocketMutation Socket(int id) => new SocketMutation(id);
+
+        public ThermostatMutation Thermostat(int id) => new ThermostatMutation(id);
+
+        public TimerMutation Timer(int id) => new TimerMutation(id);
+
+        public VariableMutation Variable(int id) => new VariableMutation(id);
     }
 }
--- src/Domotech.iRemote.WebService/GraphApi/Query.cs
+++ src/Domotech.iRemote.WebService/GraphApi/Query.cs
@@ -14,22 +14,34 @@ namespace Domotech.iRemote.WebService.GraphApi
                 DownloadProgressInPercent = connectionStateService.GetDownloadProgressInPercent(),
             };
 
-        public Light[] Lights([Service] IClient client)
-            => client.Lights().Select(Light.Create).ToArray();
+        public AudioSource[] AudioSources([Service] IClient client)
+            => client.AudioSources().Select(AudioSource.Create).ToArray();
+
+        public AudioZone[] AudioZones([Service] IClient client)
+            => client.AudioZones().Select(AudioZone.Create).ToArray();
 
         public DimmableLight[] DimmableLights([Service] IClient client)
             => client.Dimmers().Select(DimmableLight.Create).ToArray();
 
-        public Socket[] Sockets([Service] IClient client)
-            => client.Sockets().Select(Socket.Create).ToArray();
-
-        public Shutter[] Shutters([Service] IClient client)
-            => client.Shutters().Select(Shutter.Create).ToArray();
+        public Light[] Lights([Service] IClient client)
+            => client.Lights().Select(Light.Create).ToArray();
 
         public Scene[] Scenes([Service] IClient client)
             => client.Scenarios().Select(Scene.Create).ToArray();
 
+        public Shutter[] Shutters([Service] IClient client)
+            => client.Shutters().Select(Shutter.Create).ToArray();
+
+        public Socket[] Sockets([Service] IClient client)
+            => client.Sockets().Select(Socket.Create).ToArray();
+
         public Thermostat[] Thermostats([Service] IClient client)
             => client.Rooms().Select(Thermostat.Create).ToArray();
+
+        public Timer[] Timers([Service] IClient client)
+            => client.Timers().Select(Timer.Create).ToArray();
+
+        public Variable[] Variables([Service] IClient client)
+            => client.LogVars().Select(Variable.Create).ToArray();
     }
 }
